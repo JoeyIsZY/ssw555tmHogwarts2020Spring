e9eebb960a42ba72687253815fc3f9777120fe5c
JoeyIsZY <1933920443@qq.com>
2020-02-26 02:48:13 -0500

    Update us01_test.py

--- us01_test.py
+++ us01_test.py
@@ -18,8 +18,8 @@ class Testcurrent_date_check(unittest.TestCase):
         test.update_families()
         self.assertEqual(current_date_check(test), [('ERROR', 'INDIVIDUAL', 'US01', 119, '@I_W_US01_1@', 'Birthday 2021-01-01 occurs in the future.'), 
                                                     ('ERROR', 'INDIVIDUAL', 'US01', 127, '@I_W_US01_2@', 'DEATH 2081-12-30 occurs in the future.'), 
-                                                    ('ERROR', 'FAMILY', 'US01', 170, '@F_W_US01_1@', 'Marriage date 2040-02-21 occurs in the future.'), 
-                                                    ('ERROR', 'FAMILY', 'US01', 172, '@F_W_US01_1@', 'Divorce date 2070-01-01 occurs in the future.')])
+                                                    ('ERROR', 'FAMILY', 'US01', 201, '@F_W_US01_1@', 'Marriage date 2040-02-21 occurs in the future.'), 
+                                                    ('ERROR', 'FAMILY', 'US01', 203, '@F_W_US01_1@', 'Divorce date 2070-01-01 occurs in the future.')])
 
 if __name__ == "__main__":
     unittest.main(exit = False, verbosity= 2)
