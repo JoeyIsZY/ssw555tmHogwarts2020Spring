382269e62f7383a8dbc936e5da088e57f7479de2
yhu71 <yhu71@stevens.edu>
2020-02-28 15:05:22 -0500

    Merge branch 'develop' of /Users/huying/GitHub/ssw555tmHogwarts2020Spring with conflicts.

--- us02.py
+++ us02.py
@@ -16,11 +16,13 @@ def birth_before_marriage(repo1):
         wife_id = fam.repo['WIFE']['detail']
 
         for indi in repo1.individuals.values():
+            # compare husbands birth with marriage date
             if indi.indi_id == hus_id:
                 hus_birt = indi.repo['BIRT']['detail']
                 if hus_birt != 'NA' and marr != 'NA' and marr < hus_birt:
                     error_message = f'Marriage date {str(marr.strftime("%Y-%m-%d"))} occurs before husband birth {str(hus_birt.strftime("%Y-%m-%d"))}.'
                     error.append(('ERROR', 'FAMILY', 'US02', fam.repo['MARR']['line'], fam.fam_id, error_message))
+            # compare wife birth with marriage date
             elif indi.indi_id == wife_id:
                 wife_birt = indi.repo['BIRT']['detail']
                 if wife_birt != 'NA' and marr != 'NA' and marr < wife_birt:
--- us03.py
+++ us03.py
@@ -13,6 +13,7 @@ def birth_before_death(repo1):
     for indi in repo1.individuals.values():
         birt = indi.repo['BIRT']['detail']
         deat = indi.repo['DEAT']['detail']
+        # compare individuals' birth and death
         if birt != 'NA' and deat != 'NA' and birt > deat:
             error_message = f'Death {str(deat.strftime("%Y-%m-%d"))} occurs before birth {str(birt.strftime("%Y-%m-%d"))}.'
             error.append(('ERROR', 'INDIVIDUAL', 'US03', indi.repo['DEAT']['line'], indi.indi_id, error_message))
