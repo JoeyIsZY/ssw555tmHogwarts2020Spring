fec535dd4ee8417f0a85e6d60baafc10bca65411
wuhuwuhuhu <wuhd1234@gmail.com>
2020-03-04 00:20:21 -0500

    test travis

--- userstory/us18_test.py
+++ userstory/us18_test.py
@@ -6,6 +6,7 @@ import unittest
 import os
 import sys 
 sys.path.append('../')
+print(sys.path)
 #in order to import file from different folder, we need add add a new path
 from us18 import us18_no_marriages_between_siblings
 from ssw555Prj_Hogwarts import Repository
