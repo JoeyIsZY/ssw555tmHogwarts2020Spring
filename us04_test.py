232157d3457e749365e55f2a84ad9651d7c34378
yhu71 <yhu71@stevens.edu>
2020-03-15 23:07:59 -0400

    update GED testfile

--- us04_test.py
+++ us04_test.py
@@ -18,8 +18,9 @@ class US04_TestCase(unittest.TestCase):
         self.assertEqual(us04_marriage_before_divorce(test),
                          [('ERROR', 'FAMILY', 'US04', 235, '@F_Z_US04_1@', 'Family @F_Z_US04_1@ has no marriage date. '),
                           ('ERROR', 'FAMILY', 'US04', (238, 242, 244), '@F_Z_US04_2@',
-                           'Family @F_Z_US04_2@ marriage date is after than divorce date. ')])
-
+                           'Family @F_Z_US04_2@ marriage date is after than divorce date. '),
+                         ('ERROR', 'FAMILY', 'US04', (278, 282, 284), '@F_Z_US05_2@',
+                          'Family @F_Z_US05_2@ marriage date is after than divorce date. ')])
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
--- us12_test.py
+++ us12_test.py
@@ -16,10 +16,10 @@ class Testparents_not_too_old(unittest.TestCase):
         test.update_families()
         # Get returned list sorted to match the result
         self.assertEqual(sorted(us12_parents_not_too_old(test)),
-                         [('ERROR', 'FAMILY', 'US12', 520, '@I_H_US12_1@', 'Age of husband is more than 80 when the child @I_H_US12_3@ birth on 0750-10-11.'),
-                          ('ERROR', 'FAMILY', 'US12', 520, '@I_H_US12_1@', 'Age of husband is more than 80 when the child @I_H_US12_4@ birth on 0790-10-22.'),
-                          ('ERROR', 'FAMILY', 'US12', 530, '@I_H_US12_2@', 'Age of wife is more than 60 when the child @I_H_US12_3@ birth on 0750-10-11.'),
-                          ('ERROR', 'FAMILY', 'US12', 530, '@I_H_US12_2@', 'Age of wife is more than 60 when the child @I_H_US12_4@ birth on 0790-10-22.')])
+                         [('ERROR', 'FAMILY', 'US12', 520, '@I_H_US12_1@', 'Age of husband is more than 80 when the child @I_H_US12_3@ birth on 1711-10-11.'),
+                          ('ERROR', 'FAMILY', 'US12', 520, '@I_H_US12_1@', 'Age of husband is more than 80 when the child @I_H_US12_4@ birth on 1712-10-22.'),
+                          ('ERROR', 'FAMILY', 'US12', 530, '@I_H_US12_2@', 'Age of wife is more than 60 when the child @I_H_US12_3@ birth on 1711-10-11.'),
+                          ('ERROR', 'FAMILY', 'US12', 530, '@I_H_US12_2@', 'Age of wife is more than 60 when the child @I_H_US12_4@ birth on 1712-10-22.')])
 
 
 if __name__ == '__main__':
