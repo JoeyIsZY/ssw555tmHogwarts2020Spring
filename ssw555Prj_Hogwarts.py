b32ce5b05ae004cae9b685ff9181c12479b785f4
JoeyIsZY <1933920443@qq.com>
2020-03-18 23:51:23 -0400

    update GED testfile

--- ssw555Prj_Hogwarts.py
+++ ssw555Prj_Hogwarts.py
@@ -20,6 +20,7 @@ from us18 import us18_no_marriages_between_siblings
 from us06 import us06_divorce_before_death
 from us12 import us12_parents_not_too_old
 from us10 import us10_marriage_after_14
+from us11 import us11_no_bigamy
 '''
 change_date_2020_2_11: change origin code from yz, Fangji Liang
 change_date_2020_2_17: 1.use fp.close() 2.reset dateitem's value 3.default: self.alive = True 4. add us01 5.add us07, Haodong Wu
@@ -228,6 +229,8 @@ def errors_print(repository1):
     errors_list += us12_parents_not_too_old(repository1)
     # us10 in Sprint2 by Yu Zhou  18/03/2020
     errors_list += us10_marriage_after_14(repository1)
+    # us11 in Sprint2 by Yu Zhou  18/03/2020
+    errors_list += us11_no_bigamy(repository1)
 
     pt_labels = ['Index', 'ERROR/ANOMALY', 'Data Type', 'User Story Number', 'Line', 'Error ID', 'Error Message']
     pt = PrettyTable(field_names=pt_labels)
