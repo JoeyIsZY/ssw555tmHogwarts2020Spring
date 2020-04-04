dfcda463408cc04b272a1026f539b3afaeb617ed
JoeyIsZY <1933920443@qq.com>
2020-04-04 18:46:59 -0400

    Sprint 3 update by yzhou

--- us11_test.py
+++ us11_test.py
@@ -15,10 +15,7 @@ class MyTestCase(unittest.TestCase):
         test.get_file_reader(path)
         test.update_individuals()
         test.update_families()
-        self.assertEqual(us11_no_bigamy(test), [('ERROR', 'INDIVIDUAL', 'US11', (180, 189), '@I3@', 'Husband:<@I3@> has one more spouse.'),
-                                            ('ERROR', 'INDIVIDUAL', 'US11', (189, 180), '@I3@', 'Husband:<@I3@> has one more spouse.'),
-                                            ('ERROR', 'INDIVIDUAL', 'US11', (415, 421), '@I_W_US17_1@', 'Husband:<@I_W_US17_1@> has one more spouse.'),
-                                            ('ERROR', 'INDIVIDUAL', 'US11', (421, 415), '@I_W_US17_1@', 'Husband:<@I_W_US17_1@> has one more spouse.')])
+        self.assertEqual(us11_no_bigamy(test), [('ERROR', 'INDIVIDUAL', 'US11', (180, 189), '@I3@', 'Husband:<@I3@> has one more spouse.'), ('ERROR', 'INDIVIDUAL', 'US11', (415, 421), '@I_W_US17_1@', 'Husband:<@I_W_US17_1@> has one more spouse.')])
 
 
 if __name__ == '__main__':
